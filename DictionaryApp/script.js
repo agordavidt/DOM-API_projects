a2e8d1ba9ee9004b6693db15e389d2f3c2b12a28
David Agor <agordavid14@gmail.com>
2024-06-27 00:46:01 +0100

    parts of speech, phonetics added

--- DictionaryApp/script.js
+++ DictionaryApp/script.js
@@ -13,5 +13,26 @@ btn.addEventListener('click', () => {
 
         fetch(`${url}${inpWord}`)
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => {
+                console.log(data);
+                result.innerHTML = `
+                 <div class="word">
+                <h3>${inpWord}</h3>
+                <button>
+                    <i class="fas fa-volume-up"></i>
+                </button>
+            </div>
+
+            <div class="details">
+                <p>${data[0].meanings[0].partOfSpeech}</p>
+                <p>${data[0].phonetic}</p>
+            </div>
+            <p class="word-meaning">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, exercitationem?
+            </p>
+            <p class="word-example">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, ex!
+            </p>
+                `
+            });
 })
