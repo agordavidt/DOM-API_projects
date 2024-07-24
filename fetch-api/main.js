fca189b05543348f55ee2f51dba772bd68ad6bad
David Agor <agordavid14@gmail.com>
2024-07-24 02:48:59 +0100

    add api

--- fetch-api/main.js
+++ fetch-api/main.js
@@ -4,9 +4,12 @@ document.querySelector(".getData").addEventListener("click", getData);
 
 async function getData() {
   try {
-    const res = await fetch("https://dog.ceo/api/breeds/image/random", {
-      mode: "no-cors",
-    });
+    const res = await fetch(
+      "https://covid-19-data.p.rapidapi.com/country/code?format=json&code=it",
+      {
+        mode: "no-cors",
+      }
+    );
     if (!res.ok) {
       console.log(`Response Status: ${res.status}`);
     }
@@ -19,6 +22,8 @@ async function getData() {
 
 
 getData();
+
+
 // url object
 
 const details = new URL("https://username:password@URL");
@@ -27,3 +32,30 @@ console.log(details.protocol);
 console.log(`userame: ${details.username}`);
 console.log(`password: ${details.password}`);
 console.log(`hostname: ${details.hostname}`);
+
+
+
+async function nameFunction(){
+    try{
+        const res = await fetch(url, settings);
+        if(!res.ok){
+            console.log(res.status);
+        }
+        const resData = await res.json();
+    }
+    catch (err){
+        console.log(err);
+    }
+}
+
+
+function nameF(){
+    fetch('url')
+    .then((res)=>res.json())  
+    .then((data)=>{
+        console.log(data);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+    }
