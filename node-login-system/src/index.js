30381e4f8e5935dfeac0d3aa836f6633b72e3fd0
David Agor <agordavid14@gmail.com>
2024-07-25 18:35:38 +0100

    add login

--- node-login-system/src/index.js
+++ node-login-system/src/index.js
@@ -9,6 +9,14 @@ const app = express();
 //use EJS as the view engine
 app.set('view engine', 'ejs');
 
+app.get("/", (req, res) => {
+    res.render("login");
+});
+
+app.get("/signup", (req, res) =>{
+    res.render("signup");
+})
+
 
 
 // choose port
